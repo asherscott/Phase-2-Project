ae7a5b38097fa94425d8c05d8791e704ed4b7736
asher scott <asher.scott6@gmail.com>
2022-03-30 16:19:56 -0500

    Revert "json-server, character list, charCard"
    This reverts commit a2544e9415b0ee613188f801e071ba30af8cca00.

--- src/Characters.js
+++ src/Characters.js
@@ -1,39 +1,27 @@
-import { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
-import ListItem from "./ListItem";
 import "./Characters.css";
 
 function Characters() {
-  const [characters, setCharacters] = useState([]);
-  const [displayCard, setDisplayCard] = useState({});
-
-  useEffect(() => {
-    fetch("http://localhost:3000/characters")
-      .then((res) => res.json())
-      .then((chars) => {
-        setCharacters(chars);
-        setDisplayCard(chars[0]);
-      });
-  }, []);
-
-  const displayList = characters.map((character) => {
-    return (
-      <ListItem
-        key={character.name}
-        character={character}
-        setDisplayCard={setDisplayCard}
-        displayCard={displayCard}
-      />
-    );
-  });
-
   return (
     <div id="charContent">
       <div className="charBackground"></div>
 
-      <ul id="charList">{displayList}</ul>
-
-      <CharacterCard character={displayCard} />
+      <div id="charBody">
+        <div className="charCards">
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+        </div>
+      </div>
     </div>
   );
 }
