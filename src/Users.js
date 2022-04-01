bcafcb584bb4d3b21d0fca1cc508f9b31339321e
asher scott <asher.scott6@gmail.com>
2022-04-01 10:35:04 -0500

    scrollbar, Tips/tricks style

--- src/Users.js
+++ src/Users.js
@@ -30,60 +30,29 @@ function Users() {
 
   return (
     <div className="parallax">
-      <div className="background parallax_layer parallax__layer--back">
-        <img
-          className="title"
-          id="background"
-          src={
-            "https://preview.redd.it/6sgzg1no74e41.png?width=960&crop=smart&auto=webp&s=50edef4c58b017aae1633e1db689bbfffefa59e8"
-          }
-        />
-      </div>
       <div className="image parallax_layer parallax__layer--base">
         <img
-          className="image"
+          className="image firstImg"
           src={
             "https://i.pinimg.com/564x/5b/7b/80/5b7b80e77be061cd9d621904b044692f.jpg"
           }
         />
-        <img
-          className="image"
-          src={
-            "https://preview.redd.it/fuqjemqby1951.jpg?width=640&crop=smart&auto=webp&s=8da0b9e79f9053b7f01b61fef65423c8b737595e"
-          }
-        />
         <img className="image" src={"https://i.redd.it/r1cvm4n8ank41.jpg"} />
-        <img className="image" src={"https://i.ibb.co/4NZWx6H/Minerals1.jpg"} />
-        <img className="image" src={"https://i.ibb.co/xJkGJ3q/Minerals2.jpg"} />
-        <img className="image" src={"https://i.ibb.co/DW9YTLz/Minerals3.jpg"} />
-        <img className="image" src={"https://i.ibb.co/sF4Nnyh/Minerals4.jpg"} />
-        <img
-          className="image"
-          src={"https://i.ibb.co/txdFGCg/Artifacts1.jpg"}
-        />
-        <img
-          className="image"
-          src={"https://i.ibb.co/1KRKhSg/Artifacts2.jpg"}
-        />
-        <img
-          className="image"
-          src={"https://i.ibb.co/zRhvzTq/Artifacts3.jpg"}
-        />
-        <img
-          className="image"
-          src={"https://i.ibb.co/R3rfYF7/Artifacts4.jpg"}
-        />
       </div>
 
       <div id="pageBackground">
         <div id="commentBody">
-          <button onClick={handleClick}>
-            {!showForm ? "Leave a Comment!" : "Hide Comment Form"}
-          </button>
+          <div className="showForm">
+            <button onClick={handleClick}>
+              {!showForm ? "Leave a Comment!" : "Hide Comment Form"}
+            </button>
+          </div>
+
           {showForm ? <CommentForm setComments={setComments} /> : null}
           <div id="commentSection">{displayComments}</div>
         </div>
       </div>
+      <div id="background"></div>
     </div>
   );
 }
